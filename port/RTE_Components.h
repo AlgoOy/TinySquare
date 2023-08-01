2d33625cefcf23ae243e11b8777892c9e3f9abf8
AlgoOy <2407383500@qq.com>
2023-08-01 21:47:53 +0800

    fix scons bug

---
+++ port/RTE_Components.h
@@ -0,0 +1,51 @@
+/****************************************************************************
+*  Copyright 2023 Gorgon Meducer (Email:embedded_zhuoran@hotmail.com)       *
+*                                                                           *
+*  Licensed under the Apache License, Version 2.0 (the "License");          *
+*  you may not use this file except in compliance with the License.         *
+*  You may obtain a copy of the License at                                  *
+*                                                                           *
+*     http://www.apache.org/licenses/LICENSE-2.0                            *
+*                                                                           *
+*  Unless required by applicable law or agreed to in writing, software      *
+*  distributed under the License is distributed on an "AS IS" BASIS,        *
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+*  See the License for the specific language governing permissions and      *
+*  limitations under the License.                                           *
+*                                                                           *
+****************************************************************************/
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stdint.h"
+
+//#define RTE_Acceleration_Arm_2D_Extra_Benchmark
+//    #define RTE_Acceleration_Arm_2D_Extra_Benchmark_Watch_Panel
+
+/* ARM::Acceleration:Arm-2D Extras:Controls:1.4.3 */
+#define RTE_Acceleration_Arm_2D_Extra_Controls
+/* ARM::Acceleration:Arm-2D Extras:LCD ASCII Printf:1.3.1 */
+#define RTE_Acceleration_Arm_2D_Extra_LCD_printf__
+/* ARM::Acceleration:Arm-2D Helper:Display Adapter:1.6.1 */
+#define RTE_Acceleration_Arm_2D_Helper_Disp_Adapter0
+
+/* ARM::Acceleration:Arm-2D Helper:PFB:1.7.2 */
+#define RTE_Acceleration_Arm_2D_Helper_PFB
+/* ARM::Acceleration:Arm-2D Helper:Scene:1.1.9 */
+
+/* ARM::Acceleration:Arm-2D:Alpha-Blending:1.1.4 */
+#define RTE_Acceleration_Arm_2D_Alpha_Blending
+/* ARM::Acceleration:Arm-2D:Core:1.1.4 */
+#define RTE_Acceleration_Arm_2D
+
+/* ARM::Acceleration:Arm-2D:Transform:1.1.4 */
+#define RTE_Acceleration_Arm_2D_Transform
+
+
+
+#endif /* RTE_COMPONENTS_H */
